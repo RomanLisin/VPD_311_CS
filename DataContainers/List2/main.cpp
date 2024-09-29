d987b2501b7c94811e3685b2208dc66a2fa33206
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2024-09-29 21:02:24 +1000

    doneFirstLesson

--- DataContainers/List2/main.cpp
+++ DataContainers/List2/main.cpp
@@ -24,7 +24,7 @@ class List
 			cout << "EDestructor:\t" << this << endl;
 		}
 		friend class List;
-	}*Head, *Tail;	//Объявляем два указателя на объекты класса 'Element' непосредственно после описания класса;
+	}*Head, * Tail;	//Объявляем два указателя на объекты класса 'Element' непосредственно после описания класса;
 	size_t size;	//Количество элементов списка
 public:
 	List()
@@ -36,7 +36,10 @@ public:
 	}
 	~List()
 	{
+		while (Head)pop_front();
+		while (Tail)pop_back();
 		cout << "LDestrutor:\t" << this << endl;
+
 	}
 
 	//					Adding elements:
@@ -46,21 +49,71 @@ public:
 			Head = Tail = new Element(Data);
 		else
 		{
-			//1) Создаем новый элемент:
-			Element* New = new Element(Data);
+			////1) Создаем новый элемент:
+			//Element* New = new Element(Data);
 
-			//2) Присоединяем элемент к списку:
-			New->pNext = Head;
+			////2) Присоединяем элемент к списку:
+			//New->pNext = Head;
 
-			//3) Присоединяем список к элементу:
-			Head->pPrev = New;
+			////3) Присоединяем список к элементу:
+			//Head->pPrev = New;
 
-			//4) Смещаем Голову на новый элемент:
-			Head = New;
+			////4) Смещаем Голову на новый элемент:
+			//Head = New;
+			Head = Head->pPrev = new Element(Data, Head);
 		}
 		size++;
 	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr) return push_front(Data);
+		Tail = Tail->pNext = new Element(Data, nullptr, Tail);
+		size++;
+	}
+	void insert(int Data, int Index)
+	{
+		if (Index == 0)return push_front(Data);
+			if (Index == size-1)return push_back(Data);
+			if (Index >= size) throw std::exception("Insert: Out of range ecxeption");
+		Element* Temp;
+		if (Index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < Index; i++) Temp = Temp->pNext;
 
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - Index - 1; i++) Temp = Temp->pPrev;
+		}
+		Temp->pNext->pPrev = Temp->pPrev->pNext = new Element(Data, Temp->pNext, Temp->pPrev);
+		size++;
+	}
+	//             Removing elements:
+
+	void pop_front()
+	{
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+			size=0;
+			return;
+		}
+		Head = Head->pNext;
+		delete Head->pPrev;
+		Head->pPrev = nullptr;
+		size--;
+	}
+	void pop_back()
+	{
+		if (Head == Tail) return pop_front();
+		Tail = Tail->pPrev;
+		delete Tail->pNext;
+		Tail->pNext = nullptr;
+		size--;
+	}
 	//						Methods:
 	void print()const
 	{
@@ -92,4 +145,22 @@ void main()
 	}
 	list.print();
 	list.reverse_print();
+	/*for (int i = 0; i < n*2; i++)list.pop_back();
+	list.print();
+	list.reverse_print();*/
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	try
+	{
+		list.insert(value, index);
+		list.print();
+		list.reverse_print();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << endl;
+	}
+
 }
